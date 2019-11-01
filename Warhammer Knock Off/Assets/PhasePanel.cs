9fb3fb3824b04bab1acd036bc2054b9b1daa8365
Richard <r.banks@burnley.ac.uk>
2019-11-01 10:59:19 +0000

    Made a start on UI

---
+++ Warhammer Knock Off/Assets/PhasePanel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PhasePanel : MonoBehaviour
+{
+
+	public Text PhaseDisplay;
+
+	private Animator _anims;
+
+	private void Awake()
+	{
+		_anims = GetComponent<Animator>();
+		Utilities._onPhaseChange += PopPhaseUI;
+		PhaseDisplay.text = Utilities.currentPhase.ToString() + " Phase";
+		
+	}
+
+	public void PopPhaseUI()
+	{
+		PhaseDisplay.text = Utilities.currentPhase.ToString() + " Phase";
+		_anims.SetTrigger("Pop");
+	}
+}
--- Warhammer Knock Off/Assets/Scripts/Utilities/Utilities.cs
+++ Warhammer Knock Off/Assets/Scripts/Utilities/Utilities.cs
@@ -1,13 +1,24 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 
 // Current Phase handled by the Turn Manager
 public enum Phase { PlayerMovement, EnemyMovement, Fight };
 public class Utilities
 {
+    
+    public delegate void OnPhaseChange();
+
+    public static OnPhaseChange _onPhaseChange;
+    
     public static Phase currentPhase { get; set; }
+
+    
+    
+    
 
 
     public static int RollADice(int sides)
@@ -57,6 +68,11 @@ public class Utilities
     public static void ChangePhases(Phase changeTo)
     {
         currentPhase = changeTo;
+        if (_onPhaseChange != null)
+        {
+            _onPhaseChange();
+        }
+
         Debug.Log("Changing To: " + changeTo.ToString());
     }
 }
